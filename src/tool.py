01be7561b05bdad701bcabb747a00119be0af070
ypei <ypei@cs.utexas.edu>
2016-04-29 17:43:25 -0500

    small changes

--- src/tool.py
+++ src/tool.py
@@ -1,11 +1,17 @@
+import math
 
 def gaussian(mean=0.0, std=1.0):
     pass
 
-def xavier(num_inputs, num_neurons):
-
+def xavier(*args):
+    prod = 1;
+    for i in 0:len(args)-1 :
+        prod *= args[i];
+    scale = math.sqrt(3.0/prod);
+    
 
 def zeros(num_inputs, num_neurons):
     pass
 
-def Param(name, objects, )
+class Param:
+    
