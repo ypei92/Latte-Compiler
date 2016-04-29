1fa72b4584e95a765c0108611d2588cde0bd4a6c
tianzhang1121 <tyz@utexas.edu>
2016-04-28 22:54:39 -0500

    fixed connections

--- src/layers/FullyConnectedLayer.py
+++ src/layers/FullyConnectedLayer.py
@@ -24,19 +24,25 @@ class WeightedNeuron(Neuron):
 
 def FullyConnectedLayer(name, net, input_ensemble, size):
     num_inputs = len(input_ensemble.neuron)
+
     weights = xavier(num_inputs, size)
-    gd_weights = zeros(num_inputs. size)
+    gd_weights = zeros(num_inputs, size)
 
     bias = zeros(1, size)
     gd_bias = zeros(1, size)
 
+    neurons = []
+
     for i in 1:size :
-        neurons.append(WeightedNeuron(weights, gd_weights, bias, gd_bias))
-    ens = Ensemble(net, name, neurons, [Param(name, weights, 1.0, 1.0),\ 
-                                        Param(name, bias, 2,0, 0.0)]) 
+        neurons.append(WeightedNeuron(weights[i], gd_weights[i], bias[0][i], gd_bias[0][i]))
+    ens = Ensemble(net, name, neurons, [Param(name, "weights", 1.0, 1.0),\ 
+                                        Param(name, "bias", 2,0, 0.0)]) 
 
     def mapping:
-        return [0, num_inputs]
+        indices = []
+        for i in 0:num_inputs :
+            indices.append(i)
+        return indices
 
     add_connections(net, input_ensemble, ens, mapping)
     return ens
