b952caf634dcfb7139b990eb107f524c7226c75d
tianzhang1121 <tyz@utexas.edu>
2016-05-01 17:16:22 -0500

    added recurrent attribute to connection

--- src/connection.py
+++ src/connection.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 class Connection:
-    def __init__(self, ensemble, function = null):
+    def __init__(self, ensemble, function = null, recurrent = false):
         self.source = ensemble
         self.mapping = function
+        self.recurrent = recurrent
 
-def add_connections(net, src, sink, mapping):
-    sink.connections.append(Connection(src, mapping))
+def add_connections(net, src, sink, mapping, recurrent = false):
+    sink.connections.append(Connection(src, mapping, recurrent))
 	
