a68fd484e53e017efbabfbb806b620e55c81997b
ypei <ypei@cs.utexas.edu>
2016-05-05 17:29:42 -0500

    added header

--- src/astvisitor.py
+++ src/astvisitor.py
@@ -1,10 +1,29 @@
+import sys                                                                          
+sys.path.append('./stdlib')
+sys.path.append('./stdlib/layers')
+sys.path.append('../userdef/')
+
+from neuron import *
+from conncection import *
+from ensemble import *
+from network import *
+from EmbedIDLayer import *
+from GRULayer import *
+from MemoryDataLayer import *
+from FullyConnectedLayer import *
+from InnerProductLayer import *
+from SoftmaxLossLayer import *
+from tools import *
 import ast
 import numpy as np
-import math
-import neuron
 
 def main():
-    pass
+    n = Neuron()
+    #top = ast.parse(neuron)
+    #print ast.dump(top)
 
-
+if __name__ == "__main__":
+    main()
+    
+    
 
